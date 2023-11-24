234f6e75502f0df427591c272816b2efa2540d89
TranThienDatLaGi <tranthiendatlagi@gmail.com>
2023-11-25 02:24:06 +0700

    upload khong co thong ke

--- src/main/java/vn/edu/iuh/fit/backend/repositories/OrderRepository.java
+++ src/main/java/vn/edu/iuh/fit/backend/repositories/OrderRepository.java
@@ -9,13 +9,21 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    @Query("SELECT o.orderDate as orderDate, count(o.orderDate) as totalOrderNumber from Order o group by o.orderDate")
-    public List<OrderCount> statictisOrderByDay();
-    @Query("select sum(od.quantity * od.price) from Order o join o.orderDetails od" +
-            " where o.orderDate between :beginDate and :endDate group by o.order_id")
-    public double getTotalPriceByBeginDateAndEndDate(@Param("beginDate") LocalDateTime beginDate
-            , @Param("endDate") LocalDateTime endDate);
-    @Query("select sum(od.quantity * od.price) from Order o join o.orderDetails od where o.employee.id = :empID" +
-            " group by o.employee.id")
-    public double getTotalPriceByEmployeeID(@Param("empID") long employeeID);
+    @Query("SELECT avg(s.total) FROM (SELECT sum(od.quantity * od.price) as total FROM Order o JOIN o.orderDetails od group by Month(o.orderDate), year(o.orderDate)) s")
+    Double calcAvgRevenueByMonth();
+
+    @Query("SELECT avg(s.total) FROM (SELECT sum(od.quantity * od.price) as total FROM Order o JOIN o.orderDetails od group by year(o.orderDate)) s")
+    Double calcAvgRevenueByYear();
+
+    @Query("SELECT Month(o.orderDate), year(o.orderDate), sum(od.quantity * od.price) FROM Order o join o.orderDetails od group by Month(o.orderDate), year(o.orderDate) order by year(o.orderDate) desc, month(o.orderDate) desc limit 12")
+    List<Object[]> calcRevenueByMonth();
+
+    @Query("SELECT day(o.orderDate), sum(od.quantity * od.price) FROM Order o join o.orderDetails od where year(o.orderDate) = :year and month(o.orderDate) = :month group by day(o.orderDate)")
+    List<Object[]> calcRevenueByDay(@Param("year") int year, @Param("month")  int month);
+
+    @Query("SELECT cast(o.orderDate as DATE), sum(od.quantity * od.price) FROM Order o join o.orderDetails od where o.orderDate >= :startDate and o.orderDate <= :endDate group by cast(o.orderDate as DATE)")
+    List<Object[]> calcRevenueByTimePeriod(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
+
+    @Query("SELECT o.employee, sum(od.quantity * od.price) FROM Order o join o.orderDetails od where o.orderDate >= :startDate and o.orderDate <= :endDate group by o.employee")
+    List<Object[]> calcRevenueByEmployeeInTimePeriod(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
--- src/main/java/vn/edu/iuh/fit/backend/services/OrderService.java
+++ src/main/java/vn/edu/iuh/fit/backend/services/OrderService.java
@@ -6,19 +6,76 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import vn.edu.iuh.fit.backend.dto.EmployeeDTO;
+import vn.edu.iuh.fit.backend.models.Employee;
 import vn.edu.iuh.fit.backend.models.Order;
 import vn.edu.iuh.fit.backend.repositories.OrderRepository;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.YearMonth;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 @Service
 public class OrderService {
-    private OrderRepository orderRepository;
     @Autowired
-    public OrderService(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
-    }
+    private OrderRepository orderRepository;
+
     public Page<Order> findAll(int pageNo, int sizeNo, String sortBy, String sortDirection){
         Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
         Pageable pageable = PageRequest.of(pageNo, sizeNo, sort);
         return orderRepository.findAll(pageable);
+    }
+    public Map<YearMonth, Double> calcRevenueByMonth() {
+        List<Object[]> list = orderRepository.calcRevenueByMonth();
+
+        Collector<Object[], ?, TreeMap<YearMonth, Double>> map = Collectors.toMap((Object[] o) -> YearMonth.of(((Number) o[1]).intValue(), ((Number) o[0]).intValue()),
+                (Object[] o) -> ((Number) o[2]).doubleValue(),
+                Double::sum,
+                TreeMap::new);
+
+        return list.parallelStream().collect(map);
+    }
+
+    public Map<Integer, Double> calcRevenueByDay() {
+        YearMonth yearMonth = YearMonth.now();
+        List<Object[]> list = orderRepository.calcRevenueByDay(yearMonth.getYear(), yearMonth.getMonthValue());
+
+        Collector<Object[], ?, TreeMap<Integer, Double>> collector = Collectors.toMap((Object[] o) -> ((Number) o[0]).intValue(),
+                (Object[] o) -> ((Number) o[1]).doubleValue(),
+                Double::sum,
+                TreeMap::new);
+
+        return list.parallelStream().collect(collector);
+    }
+
+    public Map<LocalDate, Double> calcRevenueByTimePeriod(LocalDate startDate, LocalDate endDate) {
+        return orderRepository.calcRevenueByTimePeriod(
+                LocalDateTime.of(startDate, LocalTime.MIN),
+                LocalDateTime.of(endDate, LocalTime.MAX)
+        ).parallelStream().collect(Collectors.toMap(
+                (Object[] o) -> ((Date) o[0]).toLocalDate(),
+                (Object[] o) -> ((Number) o[1]).doubleValue()
+        ));
+    }
+
+    public Map<EmployeeDTO, Double> calcRevenueByEmployeeInTimePeriod(LocalDate startDate, LocalDate endDate) {
+        return orderRepository.calcRevenueByEmployeeInTimePeriod(
+                LocalDateTime.of(startDate, LocalTime.MIN),
+                LocalDateTime.of(endDate, LocalTime.MAX)
+        ).parallelStream().collect(Collectors.toMap(
+                (Object[] o) -> {
+                    Employee employee = (Employee) o[0];
+
+                    return new EmployeeDTO(employee.getId(), employee.getFullname());
+                },
+                (Object[] o) -> ((Number) o[1]).doubleValue()
+        ));
     }
 }
